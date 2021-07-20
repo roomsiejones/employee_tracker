dc29e5cb1435a2d9fa9f73caa7cd2455e2251c46
Roomsie Jones <seangravito@gmail.com>
2021-07-20 18:35:33 -0500

    created switch statement to handle the responses from inquirer and created the placeholder functions with some console.logs to show they are working.

--- index.js
+++ index.js
@@ -30,11 +30,74 @@ const connection = mysql.createConnection({
         choices:["View Department", "View Roles", "View Employees", "Add Department", "Add Roles", "Add Employees", "Update Employee's Role"],
 
       })
-      // .then(res) => {
+      .then((res) => {
+        switch(res.direction){
+          case "View Department":
+            console.log("You chose to view the Departments...\n");
+            viewDepartment();
+            break;
+          case "View Roles":
+            console.log("You chose to view Roles...\n");
+            viewRoles();
+            break;
+          case "View Employees":
+            console.log("You chose to view the Employees...\n");
+            viewEmployees();
+            break;
+          case "Add Department":
+            console.log("You chose to add a Department...\n");
+            addDepartment();
+            break;
+          case "Add Roles":
+            console.log("You chose to add a Role...\n");
+           addRole();
+            break;
+          case "Add Employees":
+            console.log("You chose to add an Employee...\n");
+            addEmployee();
+            break;
+          case "Update Employee's Role":
+            console.log("You chose to update and Employee's Role...\n");
+            updateRole();
+            break;
+          default:
+            break;
+          }
 
-      // }
+      }
 
-    }
+      )};
+
+    function viewDepartment () {
+      console.log("yes this worked vDEP...\n")
+    };
+
+    function viewRoles () {
+      console.log("yes this worked vRoles...\n")
+    };
+
+    function viewEmployees () {
+      console.log("yes this worked vEmp...\n")
+    };
+
+    function addDepartment () {
+      console.log("yes this worked aDep...\n")
+    };
+
+    function addRole () {
+      console.log("yes this worked aRole...\n")
+    };
+
+    function addEmployee () {
+      console.log("yes this worked aEM...\n")
+    };
+
+    function updateRole () {
+      console.log("yes this worked upda...\n")
+    };
+
+
+
 
 
     function beginning () {
