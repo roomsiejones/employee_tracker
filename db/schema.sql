ff369dac7940e4f8e2253eac7ad38ae884fdd728
Roomsie Jones <seangravito@gmail.com>
2021-07-20 17:55:30 -0500

    changed connection.js to index.js and moved it out of db folder, added the other two tables that will be necessary for the db

--- db/schema.sql
+++ db/schema.sql
@@ -13,3 +13,28 @@ CREATE TABLE department (
 INSERT INTO department (name)
 VALUES ("Administration"), ("Instructional"), ("Cafeteria"), ("Janitorial");
 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Principal', 120000, 1), ('Assistant Principal', 100000, 1), ("Counselor", 50000, 1), ("Secretary", 30000, 1);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jonathon', 'Renaud', 1, null), ('Madame', 'McCroan', 2, 1), ('George', 'McPhearney', 3, 1), ('Lord', 'Titherton', 4, 1); 
